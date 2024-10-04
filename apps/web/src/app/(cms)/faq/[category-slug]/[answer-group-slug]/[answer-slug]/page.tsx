9342853f9d28e91bd674956fa940c255ee88dc13
LufyCZ <me@lufy.cz>
2024-10-04 12:31:29 +0200

    fix(apps/faq): handle ghost fetch fail

--- apps/web/src/app/(cms)/faq/[category-slug]/[answer-group-slug]/[answer-slug]/page.tsx
+++ apps/web/src/app/(cms)/faq/[category-slug]/[answer-group-slug]/[answer-slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getFaqAnswer } from '@sushiswap/graph-client/strapi'
+import { notFound } from 'next/navigation'
 import { getGhostBody } from 'src/app/(cms)/lib/ghost/ghost'
 
 export const revalidate = 3600
@@ -6,9 +7,17 @@ export const revalidate = 3600
 export default async function AnswerPage({
   params,
 }: { params: { 'answer-slug': string } }) {
-  const answer = await getFaqAnswer({ slug: params['answer-slug'] })
+  let answer
+  let body
 
-  const { html: body } = await getGhostBody(answer.ghostSlug)
+  try {
+    answer = await getFaqAnswer({ slug: params['answer-slug'] })
+
+    const { html } = await getGhostBody(answer.ghostSlug)
+    body = html
+  } catch {
+    return notFound()
+  }
 
   return (
     <div
